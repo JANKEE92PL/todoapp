462ca6617734d476814368f1f17ba1af13230756
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-05-16 15:47:46 +0200

    finish logic for count todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,26 +1,20 @@
 import React from "react";
-import { useState } from "react";
 
-const Todo = ({ description, done }) => {
-  const [isdone, setDone] = useState(done);
-
-  const changeTodo = () => {
-    if (isdone) {
-      setDone(false);
-    } else {
-      setDone(true);
-    }
-  };
-
+const Todo = ({ description, done, onChangeTodo, index }) => {
   return (
     <div
       className={
-        isdone
+        done
           ? "flex justify-between items-center p-2 bg-green-600 text-white"
           : "flex justify-between items-center p-2 bg-red-500 text-white"
       }
     >
-      <h1 className="text-lg cursor-pointer" onClick={changeTodo}>
+      <h1
+        className="text-lg cursor-pointer"
+        onClick={() => {
+          onChangeTodo(index);
+        }}
+      >
         {description}
       </h1>
       <button className="text-lg bg-gray-400 p-2 text-white">Löschen</button>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,43 @@
 import React from "react";
 import Todo from "./Todo";
+import { useState, useEffect } from "react";
 
-const todos = [
+const alltodos = [
   { description: "Einkaufen", done: false },
   { description: "Sport", done: false },
   { description: "Programmieren", done: false },
 ];
 
 const TodoList = () => {
+  const [opencount, countOpenTodos] = useState(0);
+  const [todos, setTodos] = useState(alltodos);
+
+  const countOpen = () => {
+    const doneTodos = todos.filter((item) => {
+      return !item.done;
+    });
+    countOpenTodos(doneTodos.length);
+  };
+
+  const changeTodo = (index) => {
+    const newTodos = [...todos];
+    if (newTodos[index].done) {
+      newTodos[index].done = false;
+    } else {
+      newTodos[index].done = true;
+    }
+    setTodos(newTodos);
+  };
+
+  useEffect(() => {
+    countOpen();
+  }, [todos]);
+
   return (
     <div className="shadow-sm hover:shadow-lg">
       <div className="text-center bg-gray-900 text-white text-3xl py-4 font-semibold">
         <h1>To do List</h1>
+        <h2>Offene Todos: {opencount}</h2>
       </div>
       {todos.map((item, index) => {
         return (
@@ -19,6 +45,8 @@ const TodoList = () => {
             description={item.description}
             done={item.done}
             key={index}
+            index={index}
+            onChangeTodo={changeTodo}
           ></Todo>
         );
       })}
